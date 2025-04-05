bf6e5e049c975e9e54bceca1cc5ff79b52f55789
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-04-05 20:17:30 +0300

    Add env variable

--- server.js
+++ server.js
@@ -1815,7 +1815,7 @@
 
     const server = http__default['default'].createServer(requestHandler(plugins, services));
 
-    const port = 3030;
+    const port = process.env.PORT || 3030;
 
     server.listen(port);
 
